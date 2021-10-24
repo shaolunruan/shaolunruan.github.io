30f901e62b171ca36fda714d256c7f6cbacf0e11
shaolun Ruan <haywardryan@foxmail.com>
2021-10-24 17:16:31 +0800

    Site updated: 2021-10-24 17:16:30

--- urls.rb
+++
@@ -1,8 +0,0 @@
-require 'net/http'
-urls = ['https://shaolunruan.github.io/', 'https://shaolunruan.github.io/index.html']
-uri = URI.parse('http://data.zz.baidu.com/urls?site=https://shaolunruan.github.io&token=FoxIKuSowFWd1JhG')
-req = Net::HTTP::Post.new(uri.request_uri)
-req.body = urls.join("\n")
-req.content_type = 'text/plain'
-res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
-puts res.body
